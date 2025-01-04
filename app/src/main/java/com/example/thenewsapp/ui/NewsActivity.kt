5d435739c2aed9a91c24f4b4de53ca30ab5efddd
Dyvid12 <vitra1812@gmail.com>
2025-01-04 20:57:33 +0700

    add profileFragment

--- app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -23,7 +23,6 @@ class NewsActivity : AppCompatActivity() {
         val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
         newsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
